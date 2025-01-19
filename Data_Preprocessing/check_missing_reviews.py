864b5ea260d97bd82140176598d053b61af40924
NaveejaSajeevan <naveejasajeevan@gmail.com>
2025-01-19 21:34:52 +0530

    LLama generated review added for ICLR

---
+++ Data_Preprocessing/check_missing_reviews.py
@@ -0,0 +1,107 @@
+import re
+import os
+import json
+import sys
+ai_generation_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../AI_generation"))
+# Add AI_generation to sys.path
+sys.path.append(ai_generation_path)
+
+from utils import *
+
+def remove_markdown(text):
+    text = re.sub(r"#+\s*", "", text) # Remove headings (###, ##, #)
+    text = re.sub(r"\*\*(.*?)\*\*", r"\1", text)  # Remove **bold**
+    text = re.sub(r"\*(.*?)\*", r"\1", text)  # Remove *italic*
+    text = re.sub(r"_(.*?)_", r"\1", text)  # Remove _italic_
+    text = re.sub(r"\n{2,}", "\n", text)  # Reduce multiple newlines to 1
+    text = text.strip() 
+    return text
+
+
+def check_reviews(rev_type):
+    output_dir = "./cleandata"
+    if rev_type == "human":
+        models = ["reviews"]
+        human = True
+    else:
+        models = ['meta-llama-Llama-3.3-70B-Instruct']
+        human = False
+        
+    for each_conf in data_dir:
+        print(f"\n\nProcessing :{each_conf}")
+        for f in folders:
+            devpath = os.path.join(each_conf,f) #../data/acl_2017/dev
+            print(devpath)
+            for model in models:
+                print(f"Model : {model}")
+                modelpath = os.path.join(devpath,model) #../data/acl_2017/dev/meta-llama-Llama-3.3-70B-Instruct
+                if not human: #llm
+                    for each_level in os.listdir(modelpath):  #level1, level2, level3, level4
+                        levelpath = os.path.join(modelpath,each_level) #../data/acl_2017/dev/meta-llama-Llama-3.3-70B-Instruct/level1
+                        count_review = 0
+                        for each_json in os.listdir(levelpath):#list of jsonfiles
+                            json_name =each_json.split('.')[0]
+                            inputpath = os.path.join(levelpath,each_json)#....file.json
+                            
+                            with open(inputpath, "r", encoding='utf-8') as file:
+                                data = json.load(file)
+                            # if "iclr" in each_conf:
+                            #     data = preprocess_iclr(data)
+                            # Extract comments
+                            comments = [review["comments"] for review in data["reviews"]]
+                            count_review +=len(comments)
+                            review_count = 1
+                            # print(f"inputpath :{inputpath}")
+                            for each_review in comments:
+
+                                # cleaned = remove_markdown(each_review)
+                                # cleaned = fix_text(cleaned)
+                                
+                                # output_path = levelpath.replace("../data", output_dir)
+                                # os.makedirs(output_path, exist_ok=True)
+                                # output_path = os.path.join(output_path,json_name+f"_{review_count}"+".txt")
+                                # # print(f"output_path :{output_path}")
+                                # with open(output_path,'w', encoding="utf-8") as f:
+                                #     f.write(cleaned)
+                                # review_count += 1
+                                if not each_review:
+                                    print(f"{inputpath} contains empty reviews")
+                        print(f"#reviews in {each_level}: {count_review}")
+                # else:
+                #     for each_json in os.listdir(modelpath):#list of jsonfiles
+                #         json_name =each_json.split('.')[0]
+                #         inputpath = os.path.join(modelpath,each_json)#....file.json
+                #         with open(inputpath, "r", encoding='utf-8') as file:
+                #             data = json.load(file)
+                #         if "iclr" in each_conf:
+                #             data = preprocess_iclr(data)
+                #         # Extract comments
+                #         comments = [review["comments"] for review in data["reviews"]]
+                #         review_count = 1
+                #         # print(f"inputpath :{inputpath}")
+                #         for each_review in comments:
+
+                #             cleaned = remove_markdown(each_review)
+                #             cleaned = fix_text(cleaned)
+                            
+                #             output_path = modelpath.replace("../data", output_dir)
+                #             os.makedirs(output_path, exist_ok=True)
+                #             output_path = os.path.join(output_path,json_name+f"_{review_count}"+".txt")
+                #             # print(f"output_path :{output_path}")
+                #             with open(output_path,'w', encoding="utf-8") as f:
+                #                 f.write(cleaned)
+                #             review_count += 1
+                    
+    
+if __name__ == "__main__":
+    data_dir = [ "../data/acl_2017/",
+                 "../data/conll_2016/",
+                  "../data/iclr_2017/",
+                   "../data/nips_2013-2017/2013/",
+                    "../data/nips_2013-2017/2014/",
+                     "../data/nips_2013-2017/2015/",
+                      "../data/nips_2013-2017/2016/",
+                       "../data/nips_2013-2017/2017/"]
+    folders = ['dev','test','train']
+
+    check_reviews("llm") 
